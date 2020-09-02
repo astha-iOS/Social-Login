a61d07d62ef052a2037052e617178fdb5a6dcdeb
Wdp <wdpmobileteam@gmail.com>
2020-09-02 12:45:49 +0530

    Initial Commit

---
+++ SocialsLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialsLogin
+//
+//  Created by WDP on 02/09/20.
+//  Copyright © 2020 WDP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
